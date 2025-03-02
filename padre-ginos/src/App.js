1b05f16ee1f41230f07eee5b31366e8a7286992a
Rosen Toshev <rtoscheff@gmail.com>
2025-03-02 16:38:42 +0000

    Added JSX to padre ginos

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
+import Pizza from "./Pizza";
 
 const App = () => {
-  debugger;
   return React.createElement("div", {}, [
     React.createElement(Pizza, {
       name: "The Pepperoni Pizza",
